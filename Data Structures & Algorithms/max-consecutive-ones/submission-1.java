c77c9d97ba8961299ffa8a1692fd74711c976ed8
Zurab Chkhartishvili <zchkharta@gmail.com>
2026-06-08 19:17:51 -0400

    Add: max-consecutive-ones - submission-1

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int m = 0;
+        int a = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == 0) { a = 0;}
+            else a += 1;
+            m = Math.max(a, m);
+        }
+        return m;
+
+    }
+}
